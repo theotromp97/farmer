af0b1adab560a1d5474ca8199174e71e2660f398
theotromp97 <35178139+theotromp97@users.noreply.github.com>
2020-12-20 22:19:46 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,1 @@
-
-function create_GUI(){
-	
-div = document.getElementById("content_value").getElementsByClassName("vis")[4];
-div.insertAdjacentHTML("afterend",   '<div class="farmscript"> <table> <tbody> <tr> <td>Amount of farms=</td> <td><input id="amount_id" value="100"></td> </tr> <tr>auto farmer</tr> <tr> <td>smart farming </td> <td><input id="smart_farming_checkbox_id" type="checkbox" checked="true"></td> </tr> <tr> <td>LA enhancer active</td> <td><input id="LA_enhancer_active_checkbox_id" type="checkbox"></td> </tr> <tr> <td><button id="button_start_farm" onclick="start()">start </button></td> </tr> </tbody> </table></div>');
- 
-}
-create_GUI();
+console.log("test");
